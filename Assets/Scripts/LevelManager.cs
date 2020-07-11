03ea8d4498c8bda7c3bac3e92ae141dc1480b034
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2020-07-11 21:14:51 +0100

    Ja move e ja faz merdas

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private int _inputsAmount;
+    [SerializeField] private Slider _commandsSlider; 
     private Queue<InputData> _playerInputs;
     [Tooltip("called when the turn for the player to pick his actions ends")]
     public UnityEngine.Events.UnityEvent OnTurnSwitch;
     public bool PlayerTurn{get; private set;}
     public int MaxInputs => _inputsAmount;
     public int CurrentInputs => _playerInputs.Count;
-    public float Percent => CurrentInputs / MaxInputs;
+    public float Percent => (float)CurrentInputs / (float)MaxInputs;
     public Queue<InputData> PlayerInputs
     {
         get
         {
-            if (_playerInputs == null) _playerInputs = new Queue<InputData>(_inputsAmount);
             return _playerInputs;
         }
     }
@@ -38,7 +39,15 @@ public class LevelManager : MonoBehaviour
         }
     }
 
+    private void Awake() {
+        _playerInputs = new Queue<InputData>(_inputsAmount);
+    }
+
     private void Start() {
         PlayerTurn = true;
+    }
+
+    private void Update() {
+        _commandsSlider.SetValueWithoutNotify(Percent);
     }
 }
