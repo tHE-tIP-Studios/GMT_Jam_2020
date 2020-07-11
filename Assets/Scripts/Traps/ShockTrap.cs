3cbd646e9c0ad433e104009708376ee4d5612eab
Miguel Romao <miguelrf95@gmail.com>
2020-07-11 16:45:37 +0100

    Create base trap class and shock trap script

---
+++ Assets/Scripts/Traps/ShockTrap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ShockTrap : Trap
+{
+    /// <summary>
+    /// Method to be called when player enters the trap collider
+    /// </summary>
+    public override void TrapEvent(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+        //TODO Access NextInput property, and change Key value to the inverse of the selected
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerController p = GetComponent<PlayerController>();
+            OnPlayerEnter?.Invoke();
+            //TODO Add TrapEvent() to NextInput onInput action
+        }
+    }
+}
--- Assets/Scripts/Traps/Trap.cs
+++ Assets/Scripts/Traps/Trap.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
-public abstract class Trap
+[RequireComponent(typeof(BoxCollider2D))]
+public abstract class Trap : MonoBehaviour
 {
-    // public UnityEvent OnPlayerEnter;
+    protected BoxCollider2D _triggerZone = default;
+
+    protected virtual void Awake()
+    {
+        _triggerZone = GetComponent<BoxCollider2D>();
+        _triggerZone.isTrigger = true;
+        _triggerZone.size = new Vector2(1f, 1.2f);
+        _triggerZone.offset = new Vector2(0f, 0.1f);
+    }
+
+    public abstract void TrapEvent(PlayerController player);
+    public UnityEvent OnPlayerEnter;
 }
