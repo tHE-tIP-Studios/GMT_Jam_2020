dc75250d256b634e492146774020e73af0c7a823
Miguel Romao <miguelrf95@gmail.com>
2020-07-11 19:10:15 +0100

    Add SceneLoader script from other project

--- Assets/Scripts/AppManage.cs
+++ Assets/Scripts/AppManage.cs
@@ -3,22 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class AppManage : MonoBehaviour
 {
+
+    [SerializeField] private string _sceneToLoad = default;
+
     /// <summary>
     /// Method to simply load next scene in build index
     /// </summary>
     public void LoadNextScene()
     {
-        //TODO Should do a simple transition
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    /// <summary>
-    /// Method to load a specific scene by name
-    /// </summary>
-    /// <param name="sceneName"> Scene name to be loaded</param>
-    public void Load(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
+        SceneLoader.Load(_sceneToLoad);
     }
 
     /// <summary>
