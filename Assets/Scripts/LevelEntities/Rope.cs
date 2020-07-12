13f0e35549cba21eb2b455a57a74cafac2b0a41a
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2020-07-12 17:58:14 +0100

    Ropes can now moove wooow

--- Assets/Scripts/LevelEntities/Rope.cs
+++ Assets/Scripts/LevelEntities/Rope.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class Rope : MonoBehaviour, IClimbable
 {
     [SerializeField] private float _moveDistance;
+    [SerializeField] private float _moveDuration;
     public int Length {get;}
     private LineHandler _line;
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+    private Vector3 _current;
+    private float _inverseMoveDuration;
+    private void Awake() 
+    {
+        _endPosition = _startPosition + transform.right * _moveDistance;
+        _inverseMoveDuration = 1/_moveDuration;
+    }
     private void Start()
     {
         _line = GetComponentInChildren<LineHandler>();
@@ -14,5 +24,32 @@ public class Rope : MonoBehaviour, IClimbable
 
         collider2D.size = new Vector2(.8f, _line.LineLenght);
         collider2D.offset = new Vector2(0.0f, -_line.LineLenght/2);
+    }
+
+    public void NextPoint()
+    {
+        _current = _current == _startPosition ? _endPosition : _startPosition;
+        StartCoroutine(MoveToPoint(_current));
+    }
+
+    private IEnumerator MoveToPoint(Vector3 point)
+    {
+        float remaningDist = (transform.position - point).sqrMagnitude;
+
+        while(remaningDist > float.Epsilon)
+        {
+            Vector3 newPos = Vector3.MoveTowards(transform.position, point, _inverseMoveDuration * Time.deltaTime);
+            transform.position = newPos;
+
+            remaningDist = (transform.position - point).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 end = _startPosition + transform.right * _moveDistance;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(_startPosition, end);
     }
 }
