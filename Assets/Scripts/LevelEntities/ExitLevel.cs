9c633699eb583389e591514cfdd60181cc52cb1c
Miguel Romao <miguelrf95@gmail.com>
2020-07-12 15:21:06 +0100

    Add sounds to project

---
+++ Assets/Scripts/LevelEntities/ExitLevel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitLevel : Trap
+{
+    [SerializeField] private float _timeToWait = default;
+    private WaitForSeconds _timer = default;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    private void Start()
+    {
+        _timer = new WaitForSeconds(_timeToWait);
+    }
+
+    public override void TrapActivation(PlayerController player)
+    {
+        StartCoroutine(WaitBeforeLoadNextLevel());
+    }
+
+    private IEnumerator WaitBeforeLoadNextLevel()
+    {
+        //TODO Have a level cleared animation
+        yield return(_timer);
+        OnPlayerEnter?.Invoke();
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.TryGetComponent<PlayerController>(out PlayerController p);
+            TrapActivation(p);
+        }
+    }
+}
