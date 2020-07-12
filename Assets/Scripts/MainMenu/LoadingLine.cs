35e5d100c49d43cba327a8a64901c8b0bfac6189
Thomas the Franque <tfranque@gmail.com>
2020-07-12 14:40:48 +0100

    Designer, i call upon thee to ruin this

---
+++ Assets/Scripts/MainMenu/LoadingLine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class LoadingLine : MonoBehaviour
+{
+    private readonly Vector2 _delayRange = new Vector2(0.1f, 0.3f);
+    private readonly char[] _sequence = new char[8]
+    {
+        '|',
+        '/',
+        '-',
+        '\\',
+        '|',
+        '/',
+        '-',
+        '\\'
+    };
+    private TextMeshProUGUI _linePro;
+
+    private int _currIndex;
+    private Coroutine _crtn;
+
+    private void Awake()
+    {
+        _linePro = GetComponent<TextMeshProUGUI>();
+        _crtn = StartCoroutine(CBar());
+    }
+
+    private IEnumerator CBar()
+    {
+        UpdateText();
+        yield return new WaitForSeconds(Random.Range(_delayRange.x, _delayRange.y));
+        if (++_currIndex >= _sequence.Length)
+            _currIndex = 0;
+        _crtn = StartCoroutine(CBar());
+    }
+
+    private void UpdateText()
+    {
+        _linePro.text = _sequence[_currIndex].ToString();
+    }
+
+    private void OnDisable()
+    {
+        if (_crtn != null)
+            StopCoroutine(_crtn);
+    }
+}
---
+++ Assets/Scripts/MainMenu/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuButton : MonoBehaviour
+{ 
+    [SerializeField] private char _selectionPrefix = '>';
+    [SerializeField] private TextMeshProUGUI _textPro = default;
+
+    public void OnSelected()
+    {
+        string text = _textPro.text;
+        text = _selectionPrefix + text;
+        _textPro.text = text;
+    }
+    public void OnDeSelected()
+    {
+        string text = _textPro.text.TrimStart(_selectionPrefix);
+        _textPro.text = text;
+    }
+}
