c4025bb31b06947c42d2cb2058cc7507aecc4773
Miguel Romao <miguelrf95@gmail.com>
2020-07-12 21:16:45 +0100

    Minor changes for better build experience

---
+++ Assets/Scripts/HideCursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour
+{
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
